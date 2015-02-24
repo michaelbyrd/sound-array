ce7a9fa504f1964094c3258d0624bb0ef5b1d8ab
MichaelByrd <michaelbyrdsemail@gmail.com>
2015-02-24 17:35:59 -0500

    crazy stuff going on in here, add js function beat that takes array of freqs and plays a beat of them

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,33 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+
+$(function() {
+  $("#play").on("click", beat);
+  $("#stop").on("click", stopSounds);
+  $("#noteA").val(440);
+  $("#noteA").on("click", playSound);
+});
+
+function beat(freqs) {
+  var arr = [];
+  $.each(freqs, function(index, value) {
+    arr.push(T("sin", {freq:value, mul:0.5}));
+  });
+
+  T("perc", {r:500}, arr).on("ended", function() {
+    this.pause();
+  }).bang().play();
+}
+
+function stopSounds() {
+  T.pause();
+}
+
+function playSound(event) {
+  var f = parseFloat($(event.target).val());
+  console.log(f);
+  T("sin", {freq:f, mul:0.5}).play();
+}
+
